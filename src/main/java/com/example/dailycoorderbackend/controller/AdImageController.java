59aa7c277491bdca215380d97e22eafab31d371b
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-10 20:37:56 +0900

    taeyoung: adimagecontroller add

--- src/main/java/com/example/dailycoorderbackend/controller/AdImageController.java
+++ src/main/java/com/example/dailycoorderbackend/controller/AdImageController.java
@@ -1,2 +1,17 @@
-package com.example.dailycoorderbackend.controller;public class AdImageController {
+package com.example.dailycoorderbackend.controller;
+
+
+import com.example.dailycoorderbackend.repository.AdImageRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class AdImageController {
+
+    private final AdImageRepository adimageRepository;
+
+    public AdImageController(AdImageRepository adimageRepository) {
+        this.adimageRepository = adimageRepository;
+    }
 }
