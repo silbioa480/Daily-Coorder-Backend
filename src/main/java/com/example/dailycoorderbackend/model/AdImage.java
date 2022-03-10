7e631e5ca5273ffbef2fb4262d244a2c40572271
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-10 20:38:33 +0900

    taeyoung: AdImage ad_image_name  modify

--- src/main/java/com/example/dailycoorderbackend/model/AdImage.java
+++ src/main/java/com/example/dailycoorderbackend/model/AdImage.java
@@ -18,12 +18,16 @@ public class AdImage {
     @Column(name = "ad_image_file")
     private File ad_image_file;
 
+    @Column(name = "ad_image_name")
+    private String ad_image_name;
+
     public AdImage() {
     }
 
-    public AdImage(long ad_image_id, File ad_image_file) {
+    public AdImage(long ad_image_id, File ad_image_file, String ad_image_name) {
         this.ad_image_id = ad_image_id;
         this.ad_image_file = ad_image_file;
+        this.ad_image_name = ad_image_name;
     }
 
     public long getAd_image_id() {
@@ -40,5 +44,13 @@ public class AdImage {
 
     public void setAd_image_file(File ad_image_file) {
         this.ad_image_file = ad_image_file;
+    }
+
+    public String getAd_image_name() {
+        return ad_image_name;
+    }
+
+    public void setAd_image_name(String ad_image_name) {
+        this.ad_image_name = ad_image_name;
     }
 }
