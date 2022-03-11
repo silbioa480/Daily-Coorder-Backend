7c5dfccc7967561f46d6cb7fddbbd3c4dcb06470
Blation <kisn3089@naver.com>
2022-03-11 14:33:31 +0900

    Follow

--- src/main/java/com/example/dailycoorderbackend/repository/FollowRepository.java
+++ src/main/java/com/example/dailycoorderbackend/repository/FollowRepository.java
@@ -2,6 +2,12 @@ package com.example.dailycoorderbackend.repository;
 
 import com.example.dailycoorderbackend.model.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+ // 팔로우 데이터 가져와서 쏴주기
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    @Query("select to_user_id from Follow where from_user_id=?1")
+    Long findByFrom_user_id(@Param("from_user_id") String from_user_id );
 }
+
