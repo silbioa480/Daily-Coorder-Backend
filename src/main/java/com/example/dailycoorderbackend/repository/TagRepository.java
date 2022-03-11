ab158ff6dee2a833a052899e4fe95a2425a39239
silbioa480 <silbioa480@gmail.com>
2022-03-11 14:44:32 +0900

    find board id by tag name

--- src/main/java/com/example/dailycoorderbackend/repository/TagRepository.java
+++ src/main/java/com/example/dailycoorderbackend/repository/TagRepository.java
@@ -2,10 +2,12 @@ package com.example.dailycoorderbackend.repository;
 
 import com.example.dailycoorderbackend.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-//    @Query("select b from  tag where (tag_name like '%20대%' or tag_name like '%여성%' or tag_name like '%핫%')  t, board b  where t.board_id = b.board_id", nativeQuery=true)
-
+  @Query("select board_id from Tag where tag_name=?1")
+  Long findByTagName(@Param("tag_name") String tag_name);
 }
 
 
