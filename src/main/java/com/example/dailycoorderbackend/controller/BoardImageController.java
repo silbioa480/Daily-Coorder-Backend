06ce2adfdcf785aa44262bdec7f20ef2161872a3
INYONGSIK <kiuyyyy@naver.com>
2022-03-11 09:40:18 +0900

    용식 Board, BoardImage , BoardLike , Comment, Follow  컨트롤러추가 (Board는 수정)

--- src/main/java/com/example/dailycoorderbackend/controller/BoardImageController.java
+++ src/main/java/com/example/dailycoorderbackend/controller/BoardImageController.java
@@ -1,4 +1,66 @@
 package com.example.dailycoorderbackend.controller;
 
-public class Board_imageController {
+import com.example.dailycoorderbackend.exception.ResourceNotFoundException;
+import com.example.dailycoorderbackend.model.BoardImage;
+import com.example.dailycoorderbackend.repository.BoardImageRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/")
+public class BoardImageController {
+
+    private final BoardImageRepository boardImageRepository;
+
+
+    public BoardImageController(BoardImageRepository boardImageRepository) {
+        this.boardImageRepository = boardImageRepository;
+    }
+
+    @GetMapping("/boardImage")
+    public List<BoardImage> getAllBoardImage() {
+        return boardImageRepository.findAll();
+    }
+
+    @PostMapping("/boardImage")
+    public BoardImage createBoardImage(@RequestBody BoardImage boardImage) {
+        return boardImageRepository.save(boardImage);
+    }
+
+    @GetMapping("/boardImage/{board_image_id}")
+    public ResponseEntity<BoardImage> getBoardImageById(@PathVariable Long board_image_id) {
+        BoardImage boardImage = boardImageRepository.findById(board_image_id).
+                orElseThrow(() -> new ResourceNotFoundException("Board not exist with id: " + board_image_id));
+
+        return ResponseEntity.ok(boardImage);
+    }
+
+    @PutMapping("/boardImage/{board_image_id}")
+    public ResponseEntity<BoardImage> updateBoardImage(@PathVariable Long board_image_id, @RequestBody BoardImage changeBoardImage) {
+        BoardImage boardImage = boardImageRepository.findById(board_image_id).
+                orElseThrow(() -> new ResourceNotFoundException("Board not exist with id: " + board_image_id));
+
+        boardImage.setBoard_image_file(changeBoardImage.getBoard_image_file());
+        boardImage.setBoard_image_name(changeBoardImage.getBoard_image_name());
+
+        BoardImage updateBoardImage = boardImageRepository.save(boardImage);
+
+        return ResponseEntity.ok(updateBoardImage);
+    }
+
+    @DeleteMapping("/board/{board_image_id}")
+    public ResponseEntity<Map<String, Boolean>> deleteBoardImage(@PathVariable Long board_image_id) {
+        BoardImage boardImage = boardImageRepository.findById(board_image_id).
+                orElseThrow(() -> new ResourceNotFoundException("Board not exist with id: " + board_image_id));
+
+        boardImageRepository.delete(boardImage);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("delete", Boolean.TRUE);
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/example/dailycoorderbackend/controller/BoardLikeController.java
+++ src/main/java/com/example/dailycoorderbackend/controller/BoardLikeController.java
@@ -1,25 +1,66 @@
 package com.example.dailycoorderbackend.controller;
 
-import com.example.dailycoorderbackend.model.Board;
-import com.example.dailycoorderbackend.repository.Board_likeRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.dailycoorderbackend.exception.ResourceNotFoundException;
+import com.example.dailycoorderbackend.model.BoardLike;
+import com.example.dailycoorderbackend.repository.BoardLikeRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/")
-public class Board_likeController {
+public class BoardLikeController {
 
-    private Board_likeRepository board_likeRepository;
+    private final BoardLikeRepository boardLikeRepository;
 
-    public Board_likeController(Board_likeRepository board_likeRepository) {
-        this.board_likeRepository = board_likeRepository;
+    public BoardLikeController(BoardLikeRepository boardLikeRepository) {
+        this.boardLikeRepository = boardLikeRepository;
     }
 
-    @GetMapping("/board_like")
-    public List<Board> getAllBoard_like() {return board_likeRepository.findAll();}
+    @GetMapping("/boardLike")
+    public List<BoardLike> getAllBoard_like() {
+        return boardLikeRepository.findAll();
+    }
 
+    @PostMapping("/boardLike")
+    public BoardLike createBoardLike(@RequestBody BoardLike boardLike) {
+        return boardLikeRepository.save(boardLike);
+    }
 
+    @GetMapping("/boardLike/{board_like_id}")
+    public ResponseEntity<BoardLike> getBoardLikeById(@PathVariable Long board_like_id) {
+        BoardLike boardLike = boardLikeRepository.findById(board_like_id).
+                orElseThrow(() -> new ResourceNotFoundException("Board not exist with id: " + board_like_id));
+
+        return ResponseEntity.ok(boardLike);
+    }
+
+    @PutMapping("/boardLike/{board_like_id}")
+    public ResponseEntity<BoardLike> updateBoardLike(@PathVariable Long board_like_id, @RequestBody BoardLike changeBoardLike) {
+        BoardLike boardLike = boardLikeRepository.findById(board_like_id).
+                orElseThrow(() -> new ResourceNotFoundException("Board not exist with id: " + board_like_id));
+
+        boardLike.setBoard_id(changeBoardLike.getBoard_id());
+        boardLike.setBoard_like_user_id(changeBoardLike.getBoard_like_user_id());
+
+        BoardLike updateBoardLike = boardLikeRepository.save(boardLike);
+
+        return ResponseEntity.ok(updateBoardLike);
+    }
+
+    @DeleteMapping("/boardLike/{board_like_id}")
+    public ResponseEntity<Map<String, Boolean>> deleteBoard(@PathVariable Long board_like_id) {
+        BoardLike boardLike = boardLikeRepository.findById(board_like_id).
+                orElseThrow(() -> new ResourceNotFoundException("Board not exist with id: " + board_like_id));
+
+        boardLikeRepository.delete(boardLike);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("delete", Boolean.TRUE);
+
+        return ResponseEntity.ok(response);
+    }
 }
 
