5a2d74afb75f7c3479dab420c7cd98c3c712909f
INYONGSIK <kiuyyyy@naver.com>
2022-03-11 14:06:51 +0900

    용식

--- src/main/java/com/example/dailycoorderbackend/repository/TagRepository.java
+++ src/main/java/com/example/dailycoorderbackend/repository/TagRepository.java
@@ -4,4 +4,9 @@ import com.example.dailycoorderbackend.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+//    @Query("select b from  tag where (tag_name like '%20대%' or tag_name like '%여성%' or tag_name like '%핫%')  t, board b  where t.board_id = b.board_id", nativeQuery=true)
+
 }
+
+
+
