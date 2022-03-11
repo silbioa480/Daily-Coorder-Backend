5b66012b89dd4dbd4300e006506b0b6f0ac9d104
silbioa480 <silbioa480@gmail.com>
2022-03-11 14:42:21 +0900

    add find followed user id from follow user id

--- src/main/java/com/example/dailycoorderbackend/repository/FollowRepository.java
+++ src/main/java/com/example/dailycoorderbackend/repository/FollowRepository.java
@@ -2,6 +2,10 @@ package com.example.dailycoorderbackend.repository;
 
 import com.example.dailycoorderbackend.model.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+  @Query("select to_user_id from Follow where from_user_id=?1")
+  Long findByFromUserId(@Param("from_user_id") String from_user_id);
 }
