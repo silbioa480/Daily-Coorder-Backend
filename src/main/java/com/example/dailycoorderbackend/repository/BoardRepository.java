66b3b6d009527078428ba376b31a3720b5777b69
silbioa480 <silbioa480@gmail.com>
2022-03-11 19:04:57 +0900

    add getBoardByUserId

--- src/main/java/com/example/dailycoorderbackend/repository/BoardRepository.java
+++ src/main/java/com/example/dailycoorderbackend/repository/BoardRepository.java
@@ -2,6 +2,12 @@ package com.example.dailycoorderbackend.repository;
 
 import com.example.dailycoorderbackend.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+  @Query("select b from Board b where b.board_poster=?1")
+  List<Board> getBoardByUserId(@Param("board_poster") String board_poster);
 }
