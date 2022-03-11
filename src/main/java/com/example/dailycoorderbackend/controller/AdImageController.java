8690f82a833eb369f02d4246546ed90a6901c525
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-03-11 11:46:35 +0900

    taeyoung: adimagecontroller modify

--- src/main/java/com/example/dailycoorderbackend/controller/AdImageController.java
+++ src/main/java/com/example/dailycoorderbackend/controller/AdImageController.java
@@ -1,9 +1,13 @@
 package com.example.dailycoorderbackend.controller;
 
 
+import com.example.dailycoorderbackend.exception.ResourceNotFoundException;
+import com.example.dailycoorderbackend.model.AdImage;
 import com.example.dailycoorderbackend.repository.AdImageRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
@@ -14,4 +18,34 @@ public class AdImageController {
     public AdImageController(AdImageRepository adimageRepository) {
         this.adimageRepository = adimageRepository;
     }
+
+    @GetMapping("/api/adimage")
+    public List<AdImage> getAllImage() {
+        return adimageRepository.findAll();
+    }
+
+    @PostMapping("/api/adimage")
+    public AdImage createAdImage(@RequestBody AdImage adimage) {
+        return adimageRepository.save(adimage);
+    }
+
+    @GetMapping("/api/adimage/{ad_image_id}")
+    public ResponseEntity<AdImage> getByAdImageId(@PathVariable Long ad_image_id) {
+        AdImage adi = adimageRepository.findById(ad_image_id).orElseThrow(() -> new ResourceNotFoundException("ad_image_id not exist" + ad_image_id));
+        return ResponseEntity.ok(adi);
+    }
+
+    @PutMapping("/api/adimage/{ad_image_id}")
+    public ResponseEntity<AdImage> updateAdImage(@PathVariable Long ad_image_id, @RequestBody AdImage adimage) {
+        AdImage adi = adimageRepository.findById(ad_image_id).orElseThrow(() -> new ResourceNotFoundException("ad_image_id not exist" + ad_image_id));
+
+        adi.setAd_image_file(adimage.getAd_image_file());
+        adi.setAd_image_name(adimage.getAd_image_name());
+        adi.setAd_image_id(adimage.getAd_image_id());
+
+        AdImage updateAdi = adimageRepository.save(adi);
+
+        return ResponseEntity.ok(updateAdi);
+    }
+
 }
